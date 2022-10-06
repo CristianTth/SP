2847a775edaf9605e771480003fc04abaa4d19a8
CristianTth <115145869+CristianTth@users.noreply.github.com>
2022-10-06 12:20:12 +0300

    Update Book.h

--- Book.h
+++ Book.h
@@ -7,7 +7,7 @@ private:
 	vector<string> images;
 	vector<string> tables;
 public:
-	void Book(string title);
+	Book(string title);
 	void createNewParagraph(string paragraph);
 	void createNewImage(string image);
 	void createNewTable(string table);
