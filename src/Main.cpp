a3c37fb81de8777e34ae8303ba08774125d5b18b
CristianTth <cristian.toth01@e-uvt.ro>
2022-11-24 15:20:59 +0200

    Cleaning

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,27 +2,6 @@
 #include "../interface/Book.h"
 int main()
 {
-    // std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
-    // ImageProxy *img1 = new ImageProxy("Pamela Anderson");
-    // ImageProxy *img2 = new ImageProxy("Kim Kardashian");
-    // ImageProxy *img3 = new ImageProxy("Kirby Griffin");
-    // Section *playboyS1 = new Section("Front Cover");
-    // playboyS1->add(img1);
-    // Section *playboyS2 = new Section("Summer Girls");
-    // playboyS2->add(img2);
-    // playboyS2->add(img3);
-    // Book *playboy = new Book("Playboy");
-
-    // playboy->add(playboyS1);
-    // playboy->add(playboyS2);
-    // std::cout << "Creation of the content took: " << (std::chrono::system_clock::now() - start).count()/1000000000.0 << "s" << std::endl;
-    // start = std::chrono::system_clock::now();
-    // playboyS1->print();
-    // std::cout << "Printing of the section 1 took: " << (std::chrono::system_clock::now() - start).count()/1000000000.0 << "s" << std::endl;
-    // start = std::chrono::system_clock::now();
-    // playboyS1->print();
-    // std::cout << "Printing again the section 1 took: " << (std::chrono::system_clock::now() - start).count()/1000000000.0 << "s" << std::endl;
-
     Section *cap1 = new Section("Capitolul 1");
     Paragraph *p1 = new Paragraph("Centered");
     cap1->add(p1);
